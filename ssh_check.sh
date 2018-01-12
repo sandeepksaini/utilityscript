0f253068063cf55526cc64be1937065842a5a23f
Sandeep Kumar <sandeeprhct@gmail.com>
2018-01-12 14:19:55 +0200

    Create ssh_check.sh

---
+++ ssh_check.sh
@@ -0,0 +1,14 @@
+#!/bin/bash -x
+#Purpose: To enable clear text password login and enable root user login using ssh service
+
+ssh_file='/etc/ssh/sshd_configcp'
+per_bol=`egrep -i '^PermitRootLogin no|^#PermitRootLogin no' ${ssh_file}|wc -l`
+pwd_bol=`egrep -i '^PasswordAuthentication no|^#PasswordAuthentication no' ${ssh_file}|wc -l`
+
+if [ ${per_bol} -ge 0 ];
+then 
+        sed -i -e 's/PermitRootLogin no/PermitRootLogin yes/g' ${ssh_file}
+elif [ ${pwd_bol} -ge 0 ];
+then
+        sed -i -e 's/PasswordAuthentication no/PasswordAuthentication yes/g' ${ssh_file}
+fi
