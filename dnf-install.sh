8724e42aed8ca3f6498844be8cbfd97a2c44acc7
Sandeep Kumar <sandeeprhct@gmail.com>
2018-02-15 14:54:03 +0200

    Update dnf-install.sh

--- dnf-install.sh
+++ dnf-install.sh
@@ -5,15 +5,14 @@ dnf_link="http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64//dnf-0.
 dnf_conf_link="http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64/dnf-conf-0.6.4-2.sdl7.noarch.rpm"
 py3_link="http://springdale.math.ias.edu/data/puias/unsupported/7/x86_64/python-dnf-0.6.4-2.sdl7.noarch.rpm"
 
-#Download the packages 
+#Download the packages
 mkdir -p ${dnf_dir};cd ${dnf_dir}
-for download in ${dnf_link} ${dnf_conf} ${py3_link}
+for download in ${dnf_link} ${dnf_conf_link} ${py3_link}
 do
-  wget -q -P ${download}
+  wget -q -P ${dnf_dir} ${download}
 done
-
 yum -q -y install dnf-0.6.4-2.sdl7.noarch.rpm dnf-conf-0.6.4-2.sdl7.noarch.rpm python-dnf-0.6.4-2.sdl7.noarch.rpm
-
-printf "\n Deleting the /tmp/${dnf_dir}. Please type yes if dir is correct \n"
-rm -r /tmp/${dnf_dir}
-printf "\n Installation Successfull - \n Path of executable is:  `which dnf`"
+cd -
+printf "\n Deleting the ${dnf_dir}. Please type yes if dir is correct \n"
+rm -ir ${dnf_dir}
+printf "\n Installation Successfull - \n Path of executable is:  `which dnf` \n"
